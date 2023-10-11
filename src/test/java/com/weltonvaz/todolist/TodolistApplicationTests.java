d9fd8c4a073701189a89c55b0133d7b45f224bed
Welton Vaz <weltonvaz@gmail.com>
2023-10-11 09:49:00 -0300

    first commit

---
+++ src/test/java/com/weltonvaz/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weltonvaz.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
